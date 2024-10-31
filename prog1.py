0baa6dd5e69d308ce1f252e3297fb7de6e155505
UlayyaAqila <ulayyaqilah12@gmail.com>
2024-10-31 12:14:09 +0800

    prog1.py

--- prog1.py
+++ prog1.py
@@ -1,4 +1,4 @@
-print("nama saya qila")
-print('nim saya adalah 24241148')
+print("nama saya Ulayya Aqilah")
+print('nim saya 24241148')
 print('saya berasal dari mataram')
-print("nomer keberuntungan sy adalah 72")
+print("nomer keberuntungan saya adalah : 72")
