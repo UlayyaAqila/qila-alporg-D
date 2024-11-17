9730b9263db36a9a75a4cd2de6f3bf40e6ba3c58
UlayyaAqila <ulayyaqilah12@gmail.com>
2024-11-17 14:27:29 +0800

    Add files via upload

---
+++ tugas 6 qila.py
@@ -0,0 +1,13 @@
+
+a = 23
+t = 24
+l = 276.0
+
+
+a = int(input("masukkan nilai a:"))
+print (" nilai a adalah : ", a, type(a))
+t = int(input("masukkan nilai t:"))
+print (" nilai t adalah : ", t, type(t))
+l = float(input("masukkan nilai l:"))
+print (" nilai l adalah : ", l, type(l))
+
