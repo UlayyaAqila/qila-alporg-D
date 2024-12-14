67a61ade51f429fc6877f875fa25283ccebbf5e0
UlayyaAqila <ulayyaqilah12@gmail.com>
2024-12-14 19:22:46 +0800

    Add files via upload

---
+++ tugas 8 qila.py
@@ -0,0 +1,13 @@
+
+# suhu dalam celcius
+celcius = 48
+
+# rumus konversi suhu
+fahrenheit = (celcius * 9/5) + 32
+kelvin = celcius + 273.15
+reamur = celcius * 4/5
+
+# cetak nilai suhu 
+print("suhu dalam fahrenheit:", fahrenheit)
+print("suhu dalam kelvin:", kelvin)
+print("suhu dalam reamur:", reamur)
