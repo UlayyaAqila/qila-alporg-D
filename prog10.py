85ad1f82e059e969afd457ec38ed90d32a8d429b
UlayyaAqila <ulayyaqilah12@gmail.com>
2025-01-05 01:12:49 +0800

    Add files via upload

---
+++ prog10.py
@@ -0,0 +1,56 @@
+# perbandingan - jawaban ada 2 true false
+
+# >,  <, >=,  <=, ==,  !=, is, isnot
+
+x = 8
+
+hasil = x > 9
+print("x > 9 = ", hasil)
+
+hasil = x < 9
+print("x < 9 = ", hasil)
+
+hasil = x >= 9
+print("x >= 9 = ", hasil)
+
+hasil = x <= 9
+print("x <= 9 = ", hasil)
+
+hasil = x == 9
+print("x == 9 = ", hasil)
+
+# perbandingan - jawaban ada 2 true false
+
+# >,  <, >=,  <=, ==,  !=, is, isnot
+
+x = 8
+
+hasil = x > 9
+print("x > 9 = ", hasil)
+
+hasil = x < 9
+print("x < 9 = ", hasil)
+
+hasil = x >= 9
+print("x >= 9 = ", hasil)
+
+hasil = x <= 9
+print("x <= 9 = ", hasil)
+
+hasil = x == 9
+print("x == 9 = ", hasil)
+
+hasil = x != 9
+print("x != 9 = ", hasil)
+
+hasil = x is 9
+print("x is 9 = ", hasil)
+
+hasil = x is not 9
+print("x is not 9 = ", hasil)
+
+
+
+
+
+
